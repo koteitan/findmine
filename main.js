6e0653403af5c9593187976811125eeb90f7eabc
koteitan <ukaibutton@mac.com>
2024-02-24 07:52:41 +0900

    version 1.04./add option to search words for everyone

--- main.js
+++ main.js
@@ -1,31 +1,46 @@
-var url="";
-var prevurl="";
-
 var search = function(site, target){
-  var w = encodeURIComponent(form0.word.value);
-  localStorage.setItem(   "bsky_a", form0.   bsky_a.value);
-  localStorage.setItem("twitter_a", form0.twitter_a.value);
-  localStorage.setItem( "twilog_a", form0. twilog_a.value);
+  var url="";
   if(site=="all"){
     if(twitter_d.checked)search('twitter','findmine-twitter');
     if( twilog_d.checked)search('twilog' ,'findmine-twilog' );
     if(  nostr_d.checked)search('nostr'  ,'findmine-nostr'  );
     if(   bsky_d.checked)search('bsky'   ,'findmine-bsky'   );
   }else{
+    var w = encodeURIComponent(form0.word.value);
+    var forme = form0.me.checked;
     if(site=='nostr'){
-      url = "https://nostter.app/search?mine=ok&q=" + w;
+      if(forme){
+        url = "https://nostter.app/search?mine=ok&q=" + w;
+      }else{
+        url = "https://nostter.app/search?q=" + w;
+      }
     }else if(site=='bsky'){
-      var a = encodeURIComponent(form0.bsky_a.value);
-      url = "https://bsky.app/search?q=from:" + a + "+" + w;
+      if(forme){
+        var a = encodeURIComponent(form0.bsky_a.value);
+        url = "https://bsky.app/search?q=from:" + a + "+" + w;
+      }else{
+        var a = encodeURIComponent(form0.bsky_a.value);
+        url = "https://bsky.app/search?q=" + w;
+      }
     }else if(site=='twilog'){
       var a = encodeURIComponent(form0.twilog_a.value);
       url = "https://twilog.togetter.com/" + a + "/search?ao=a&word=" + w;
     }else if(site=='twitter'){
-      var a = encodeURIComponent(form0.twitter_a.value);
-      url = "https://twitter.com/search?q=from:" + a + "+" + w;
+      if(forme){
+        var a = encodeURIComponent(form0.twitter_a.value);
+        url = "https://twitter.com/search?q=from:" + a + "+" + w;
+      }else{
+        var a = encodeURIComponent(form0.twitter_a.value);
+        url = "https://twitter.com/search?q=" + w;
+      }
     }
     window.open(url, target);
   }
+}
+var saveuser=function(){
+  localStorage.setItem(   "bsky_a", form0.   bsky_a.value);
+  localStorage.setItem("twitter_a", form0.twitter_a.value);
+  localStorage.setItem( "twilog_a", form0. twilog_a.value);
 }
 window.onload=function(){
   var p;
@@ -36,14 +51,13 @@ window.onload=function(){
   if((p=localStorage.getItem(   "bsky_d"))!==null) form1.   bsky_d.checked = p==1;
   if((p=localStorage.getItem( "twilog_d"))!==null) form1. twilog_d.checked = p==1;
   if((p=localStorage.getItem("twitter_d"))!==null) form1.twitter_d.checked = p==1;
-  if((localStorage.getItem("primary")) =="twitter") twitter_pri.   setAttribute("selected",null);
-                                             else   twitter_pri.removeAttribute("selected");
-  if((localStorage.getItem("primary")) =="nostr"  )   nostr_pri.   setAttribute("selected",null);
-                                             else     nostr_pri.removeAttribute("selected");
-  if((localStorage.getItem("primary")) =="twilog" )  twilog_pri.   setAttribute("selected",null);
-                                             else    twilog_pri.removeAttribute("selected");
-  if((localStorage.getItem("primary")) =="bsky"   )    bsky_pri.   setAttribute("selected",null);
-                                             else      bsky_pri.removeAttribute("selected");
+  if(localStorage.getItem("primary")=="twitter") twitter_pri    .setAttribute("selected",null);
+  if(localStorage.getItem("primary")=="nostr"  )   nostr_pri    .setAttribute("selected",null);
+  if(localStorage.getItem("primary")=="twilog" )  twilog_pri    .setAttribute("selected",null);
+  if(localStorage.getItem("primary")=="bsky"   )    bsky_pri    .setAttribute("selected",null);
+  if(localStorage.getItem("whose"  )=="me")         form0.me    .setAttribute("checked",null);
+  if(localStorage.getItem("whose"  )=="everyone") form0.everyone.setAttribute("checked",null);
+
   setdisplay();
   document.getElementById("word").focus();
 }
@@ -63,6 +77,9 @@ var setdisplay=function(){
       localStorage.setItem(lsq, 0);
     }
   }
+}
+var savewhose=function(){
+  localStorage.setItem("whose", form0.me.checked?"me":"everyone");
 }
 var saveprimary=function(){
   localStorage.setItem("primary", form1.primarysel.value);
